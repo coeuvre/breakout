cb2e0f91e2b6479be2814a14d4582774af8d8724
Coeuvre Wong <coeuvre@gmail.com>
2016-02-22 02:05:21 +0000

    Update building toolchain

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
+cc=clang
+src=`pwd`/src/breakout.c
+
 clear
 
 [ ! -d "build" ] && mkdir build
 
 pushd build
 
-[ ! -f "Makefile" ] && cmake ..
-
-make
+$cc -std=c11 -W -Wall -g $src -lSDL2
 
 success=$?
 
